617bf20436a6b9a5de136621e06a66f25ccbe91a
Mohammad-Labib <friendlabib@gmail.com>
2026-06-14 00:37:24 +0600

    added id parts

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 app.get('/', (req, res) => {
@@ -88,6 +88,16 @@ async function run() {
     app.get('/api/companies', async(req,res)=>{
       const cursor = companyCollection.find();//skip(4)
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.get('/api/jobs/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query ={
+        _id: new ObjectId(id)
+      }
+
+      const result = await jobCollection.findOne(query);
       res.send(result);
     })
 
